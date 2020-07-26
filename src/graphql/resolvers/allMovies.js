f4991dd49c4d5dfcb7909c9ca6093d076bf3385d
phartenfeller <pharti@hotmail.de>
2020-07-26 17:56:20 +0200

    add all movies query

---
+++ src/graphql/resolvers/allMovies.js
@@ -0,0 +1,7 @@
+const movies = require('../../../data/movies.json');
+
+const allMovies = () => {
+  return movies;
+};
+
+module.exports = allMovies;
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,13 +1,25 @@
-const { buildSchema } = require('graphql');
+const { buildASTSchema } = require('graphql');
+const gql = require('graphql-tag');
 
-const schema = buildSchema(`
+const allMoviesResolver = require('./resolvers/allMovies');
+
+const schema = buildASTSchema(gql`
   type Query {
     hello: String
+    allMovies: [Movie]
+  }
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    runtime: Int
+    genres: [String]
   }
 `);
 
 const root = {
-  hello: () => 'Hello World!'
+  hello: () => 'Hello World!',
+  allMovies: allMoviesResolver
 };
 
 module.exports = { schema, root };
