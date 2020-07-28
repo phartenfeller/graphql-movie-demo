f56ce5381d82491ec05956e7c7351872d012ab69
phartenfeller <pharti@hotmail.de>
2020-07-28 20:49:43 +0200

    more things in setup

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./src/graphql/schema');
+
+const app = express();
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/graphql`);
+});
---
+++ src/graphql/schema.js
@@ -0,0 +1,2 @@
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const gql = require('graphql-tag');
